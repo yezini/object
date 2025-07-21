0e3fc0f0d93a8a42c7c631b2b85bca2b19528a6f
yezini <85025359+yezini@users.noreply.github.com>
2025-07-21 22:13:47 +0900

    Update MinesweeperGame.java

--- 지뢰찾기/MinesweeperGame.java
+++ 지뢰찾기/MinesweeperGame.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class MinesweeperGame {  //게임 전체 
+public class MinesweeperGame { //Game 객체 생성 및 실행 시작 
     public static void main(String[] args) {
         Game game = new Game(8, 10, 10); // 8행 10열, 지뢰 10개
         game.start();
