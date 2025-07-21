fef8ebed986ac75768b84c0185a339d37e814ef5
yezini <85025359+yezini@users.noreply.github.com>
2025-07-21 21:49:30 +0900

    Update and rename 지뢰찾기 to MinesweeperGame1.java

---
+++ MinesweeperGame1.java
@@ -0,0 +1,9 @@
+
+import java.util.*;
+
+public class MinesweeperGame {
+    public static void main(String[] args) {
+        Game game = new Game(8, 10, 10); // 8행 10열, 지뢰 10개
+        game.start();
+    }
+}
