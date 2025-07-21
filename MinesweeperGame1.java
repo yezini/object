e5716b6f423493c9c760032b648973519056c494
yezini <85025359+yezini@users.noreply.github.com>
2025-07-21 21:56:40 +0900

    Delete MinesweeperGame1.java

--- MinesweeperGame1.java
+++
@@ -1,9 +0,0 @@
-
-import java.util.*;
-
-public class MinesweeperGame {
-    public static void main(String[] args) {
-        Game game = new Game(8, 10, 10); // 8행 10열, 지뢰 10개
-        game.start();
-    }
-}
