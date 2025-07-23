05efc3ddc2a3f5b7f63ec62538771421e574e8b4
yezini <85025359+yezini@users.noreply.github.com>
2025-07-23 21:27:59 +0900

    Delete 지뢰찾기 directory

--- 지뢰찾기/MinesweeperGame.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public class MinesweeperGame { //Game 객체 생성 및 실행 시작 
-    public static void main(String[] args) {
-        Game game = new Game(8, 10, 10); // 8행 10열, 지뢰 10개
-        game.start();
-    }
-}
