52233cf7ec7c10cfe6ba4b32f1419f53fc8a38c2
yezini <yejin0608-@naver.com>
2025-07-23 21:41:41 +0900

    Revert "지뢰찾기"
    This reverts commit b830c265c5880540133892b7b174bd8ea99b8d5c.

--- test/src/MinesweeperGame.java
+++
@@ -1,7 +0,0 @@
-public class MinesweeperGame { //Game 객체 생성 및 게임 시작
-    public static void main(String[] args) {
-        Game game = new Game(8, 10, 10); // 8행 10열, 지뢰 10개
-        game.start(); //게임 시작
-    }
-}
-
